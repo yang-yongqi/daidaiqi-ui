a69fa8412e84864cd2c1defe079c3133c9a01b82
杨永琪 <3993010@qq.com>
2022-10-20 16:51:35 +0800

    项目初始化

--- src/views/console/index.vue
+++ src/views/console/index.vue
@@ -2,25 +2,60 @@
     <div>
         <div class="console-top">
             <a-row :gutter="24">
-                <a-col :xs="24" :md="12" :lg="12" :xl="6" class="m-t-15">
+                <a-col :xs="24" :sm="24" :md="24" :lg="12" :xl="6" class="m-t-15">
                     <a-card size="small" title="访问量" :bordered="false">
-                        <div>
+                        <template #extra>
+                            <a-tag color="success">日</a-tag>
+                        </template>
+                        <div class="console-top-card">
                             <div class="console-top-number">
                                 26,818.00
                             </div>
+                            <div class="console-top-trend">
+                                <span class="m-r-10">日同比 12.5%</span>
+                                <icon icon="CaretUpOutlined" style="color: #ed4014"></icon>
+                                <span class="m-l-10 m-r-10">周同比 5%</span>
+                                <icon icon="CaretDownOutlined" style="color: #19be6b"></icon>
+                            </div>
                         </div>
+                        <a-row justify="space-between" class="m-t-10">
+                            <a-col :span="12">总访问量</a-col>
+                            <a-col :span="12" class="text-r">280 万</a-col>
+                        </a-row>
                     </a-card>
                 </a-col>
-                <a-col :xs="24" :md="12" :lg="12" :xl="6" class="m-t-15">
+                <a-col :xs="24" :sm="24" :md="24" :lg="12" :xl="6" class="m-t-15">
                     <a-card size="small" title="销售额" :bordered="false">
+                        <template #extra>
+                            <a-tag color="error">月</a-tag>
+                        </template>
+                        <div class="console-top-card">
+                            <div class="console-top-number">
+                                ¥12,000
+                            </div>
+                            <a-progress
+                                :showInfo="false"
+                                :percent="86"
+                            />
+                        </div>
+                        <a-row justify="space-between" class="m-t-10">
+                            <a-col :span="12">总销售额</a-col>
+                            <a-col :span="12" class="text-r">68 万</a-col>
+                        </a-row>
                     </a-card>
                 </a-col>
-                <a-col :xs="24" :md="12" :lg="12" :xl="6" class="m-t-15">
+                <a-col :xs="24" :sm="24" :md="24" :lg="12" :xl="6" class="m-t-15">
                     <a-card size="small" title="订单量" :bordered="false">
+                        <template #extra>
+                            <a-tag color="success">日</a-tag>
+                        </template>
                     </a-card>
                 </a-col>
-                <a-col :xs="24" :md="12" :lg="12" :xl="6" class="m-t-15">
+                <a-col :xs="24" :sm="24" :md="24" :lg="12" :xl="6" class="m-t-15">
                     <a-card size="small" title="用户量" :bordered="false">
+                        <template #extra>
+                            <a-tag color="processing">总</a-tag>
+                        </template>
                     </a-card>
                 </a-col>
             </a-row>
@@ -29,8 +64,12 @@
 </template>
 <script>
 import {defineComponent} from 'vue';
+import {Icon} from "../../common/icon";
 
 export default defineComponent({
+    components: {
+        Icon,
+    },
     setup() {
         return {};
     },
@@ -38,7 +77,17 @@ export default defineComponent({
 });
 </script>
 <style>
-.console-top-number{
+.console-top-number {
     font-size: 30px;
+}
+
+.console-top-card {
+    height: 80px;
+    border-bottom: 1px solid rgb(232, 234, 236);
+}
+
+.console-top-trend {
+    height: 22px;
+    overflow: hidden;
 }
 </style>
--- src/views/monitor/index.vue
+++ src/views/monitor/index.vue
@@ -1,14 +1,11 @@
 <template>
-    <a-empty :image="simpleImage" />
 </template>
 <script>
-import { defineComponent } from 'vue';
-import { Empty } from 'ant-design-vue';
+import {defineComponent} from 'vue';
+
 export default defineComponent({
     setup() {
-        return {
-            simpleImage: Empty.PRESENTED_IMAGE_SIMPLE,
-        };
+        return {};
     },
 
 });

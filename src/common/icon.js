24d7e1b09a7fb9a1b81820796d3cbc410f51952b
杨永琪 <3993010@qq.com>
2022-10-23 11:41:08 +0800

    项目初始化

--- src/common/icon.js
+++ src/common/icon.js
@@ -1,5 +1,5 @@
 import { createVNode } from 'vue'
-import * as $Icon from '@ant-design/icons-vue'
+import * as $Icon  from '@ant-design/icons-vue'
 export const Icon = (props) => {
     const { icon } = props;
     return createVNode($Icon[icon]);
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,39 +18,66 @@
                 @finish="onFinish"
                 @finishFailed="onFinishFailed"
             >
-                <div>
-                    <a-form-item
-                        label=""
-                        name="username"
-                        :rules="[{ required: true, message: '请输入账号' }]"
-                    >
-                        <a-input size="large" v-model:value="formState.username" placeholder="请输入账号">
-                            <template #prefix>
-                                <icon style="color: #808695" icon="UserOutlined"></icon>
-                            </template>
-                        </a-input>
-                    </a-form-item>
-
-                </div>
-                <div>
-                    <a-form-item
-                        label=""
-                        name="password"
-                        :rules="[{ required: true, message: '请输入密码' }]"
-                    >
-                        <a-input-password size="large" v-model:value="formState.password" autocomplete="off" placeholder="请输入密码">
-                            <template #prefix>
-                                <icon style="color: #808695" icon="UnlockOutlined"></icon>
-                            </template>
-                        </a-input-password>
-                    </a-form-item>
-                </div>
-                <div>
-                    <a-form-item>
-                        <a-button class="login-submit" type="primary" html-type="submit">登 录</a-button>
-                    </a-form-item>
-                </div>
+                <a-form-item
+                    label=""
+                    name="username"
+                    :rules="[{ required: true, message: '请输入账号' }]"
+                >
+                    <a-input size="large" v-model:value="formState.username" placeholder="请输入账号">
+                        <template #prefix>
+                            <icon style="color: #808695" icon="UserOutlined"></icon>
+                        </template>
+                    </a-input>
+                </a-form-item>
+                <a-form-item
+                    label=""
+                    name="password"
+                    :rules="[{ required: true, message: '请输入密码' }]"
+                >
+                    <a-input-password size="large" v-model:value="formState.password" autocomplete="off"
+                                      placeholder="请输入密码">
+                        <template #prefix>
+                            <icon style="color: #808695" icon="UnlockOutlined"></icon>
+                        </template>
+                    </a-input-password>
+                </a-form-item>
+                <a-form-item>
+                    <div class="flex-between login-">
+                        <div>
+                            <a-checkbox v-model:checked="formState.remember">自动登录</a-checkbox>
+                        </div>
+                        <div>
+                            <a href="javascript:;">忘记密码</a>
+                        </div>
+                    </div>
+                </a-form-item>
+                <a-form-item>
+                    <a-button class="login-submit" type="primary" html-type="submit">登 录</a-button>
+                </a-form-item>
             </a-form>
+            <a-divider plain>其他登录方式</a-divider>
+            <a-row :gutter="20" type="flex" justify="center">
+                <a-col :span="6">
+                    <div class="text-center">
+                        <icon style="color: #2aae67;font-size: 22px" icon="WechatOutlined"></icon>
+                    </div>
+                </a-col>
+                <a-col :span="6">
+                    <div class="text-center">
+                        <icon style="color: #12b7f5;font-size: 22px" icon="QqOutlined"></icon>
+                    </div>
+                </a-col>
+                <a-col :span="6">
+                    <div class="text-center">
+                        <icon style="color: #1677ff;font-size: 22px" icon="AlipayCircleOutlined"></icon>
+                    </div>
+                </a-col>
+                <a-col :span="6">
+                    <div class="text-center">
+                        <icon style="color: #ff8200;font-size: 22px" icon="WeiboOutlined"></icon>
+                    </div>
+                </a-col>
+            </a-row>
         </div>
     </div>
 </template>
@@ -91,7 +118,7 @@ export default defineComponent({
 
     .login-logo {
         display: flex;
-        width: 180px;
+        width: 228px;
         margin: 100px auto 20px auto;
         justify-content: space-between;
 
@@ -105,12 +132,15 @@ export default defineComponent({
         color: #515a6e;
         height: 30px;
         line-height: 25px;
+        letter-spacing: 8px;
+
     }
 
     .layout-logo-desc {
         font-size: 14px;
         color: #808695;
         text-align: center;
+        letter-spacing: 8px;
     }
 
     .login-form {
@@ -118,7 +148,8 @@ export default defineComponent({
         margin: 20px auto;
     }
 }
-.login-submit{
+
+.login-submit {
     width: 100%;
     height: 40px;
     cursor: pointer;
